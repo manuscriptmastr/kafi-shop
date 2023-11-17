53003a96e2a19f68e6536182d632aeae144c65d5
Joshua Martin <the.manuscriptmaster@gmail.com>
2023-11-17 16:23:54 -0500

    Clean up Onyx

--- src/shops/onyx.ts
+++ src/shops/onyx.ts
@@ -5,17 +5,15 @@ import { Page } from 'puppeteer';
 const DOMAIN = 'https://onyxcoffeelab.com';
 
 export class Onyx extends CoffeeShop implements CoffeeShopProperties {
-  async getUrls(page: Page) {
-    await page.goto(`${DOMAIN}/collections/coffee`);
-
-    return page.$$eval(
-      'a.product-preview[href^="/products/"]',
-      (anchors: HTMLAnchorElement[]) => anchors.map((a) => a.href),
+  async getCuppingScore(page: Page) {
+    return page.$eval(
+      '[data-name="trans_cup_score"] p::-p-text(Cup Score)',
+      (p) => Number(p.parentElement!.lastElementChild!.textContent!.trim()),
     );
   }
 
-  async shouldSkipProductPage(page: Page) {
-    return !(await page.$('span.generic-option[data-value="10oz"]'));
+  async getName(page: Page) {
+    return page.$eval('h1', (h1) => h1.firstChild!.textContent!);
   }
 
   async getPrice(page: Page) {
@@ -30,21 +28,23 @@ export class Onyx extends CoffeeShop implements CoffeeShopProperties {
     return currency(priceText).value;
   }
 
-  async getName(page: Page) {
-    return page.$eval('h1', (h1) => h1.firstChild!.textContent!);
-  }
-
-  async getCuppingScore(page: Page) {
-    return page.$eval(
-      '[data-name="trans_cup_score"] p::-p-text(Cup Score)',
-      (p) => Number(p.parentElement!.lastElementChild!.textContent!.trim()),
-    );
-  }
-
   async getTastingNotes(page: Page) {
     return page.$eval(
       'div.image-features div.label::-p-text(Cup:) + div.value',
       (div) => div.textContent!.trim().split(/,\s/),
     );
+  }
+
+  async getUrls(page: Page) {
+    await page.goto(`${DOMAIN}/collections/coffee`);
+
+    return page.$$eval(
+      'a.product-preview[href^="/products/"]',
+      (anchors: HTMLAnchorElement[]) => anchors.map((a) => a.href),
+    );
+  }
+
+  async shouldSkipProductPage(page: Page) {
+    return !(await page.$('span.generic-option[data-value="10oz"]'));
   }
 }
