e5bf724577c18e67c9be1986d5b0d4a79e528524
Joshua Martin <the.manuscriptmaster@gmail.com>
2023-11-17 16:20:34 -0500

    Clean up Coffea Circulor

--- src/shops/coffea-circulor.ts
+++ src/shops/coffea-circulor.ts
@@ -5,20 +5,9 @@ import { Page } from 'puppeteer';
 const DOMAIN = 'https://coffeacirculor.com';
 
 export class CoffeaCirculor extends CoffeeShop implements CoffeeShopProperties {
-  async getUrls(page: Page) {
-    await page.goto(`${DOMAIN}/collections/all`);
-
-    while (await page.$('#section-collection a.block-fade')) {
-      const moreProductsButton = (await page.$(
-        '#section-collection a.block-fade',
-      ))!;
-      await moreProductsButton.click();
-      await wait(1000);
-    }
-
-    return page.$$eval(
-      'a.product-item:not(:has(.sold))',
-      (anchors: HTMLAnchorElement[]) => anchors.map((a) => a.href),
+  async getCountry(page: Page) {
+    return page.$eval('text/Country', (el) =>
+      el.nextElementSibling!.textContent!.trim(),
     );
   }
 
@@ -35,15 +24,50 @@ export class CoffeaCirculor extends CoffeeShop implements CoffeeShopProperties {
     return page.$eval('h1.product-title', (el) => el.textContent!);
   }
 
+  async getPrice(page: Page) {
+    const sizes = await page.$$('[data-text^="250g"] span');
+
+    const prices = [];
+
+    for (const size of sizes) {
+      await size.click();
+      const isAddToCartDisabled = await page.$eval(
+        'button[name="add"]',
+        (button) => button.disabled,
+      );
+      if (!isAddToCartDisabled) {
+        const price = await page.$eval(
+          'span.product-price',
+          (el) => +el.textContent!.slice(1).replace(',', '') / 100,
+        );
+
+        prices.push(price);
+      }
+    }
+
+    return Math.min(...prices);
+  }
+
   async getTastingNotes(page: Page) {
     return page.$eval('text/Flavor', (el) =>
       el.nextElementSibling!.textContent!.trim().split(/,\s/),
     );
   }
 
-  async getCountry(page: Page) {
-    return page.$eval('text/Country', (el) =>
-      el.nextElementSibling!.textContent!.trim(),
+  async getUrls(page: Page) {
+    await page.goto(`${DOMAIN}/collections/all`);
+
+    while (await page.$('#section-collection a.block-fade')) {
+      const moreProductsButton = (await page.$(
+        '#section-collection a.block-fade',
+      ))!;
+      await moreProductsButton.click();
+      await wait(1000);
+    }
+
+    return page.$$eval(
+      'a.product-item:not(:has(.sold))',
+      (anchors: HTMLAnchorElement[]) => anchors.map((a) => a.href),
     );
   }
 
@@ -69,29 +93,5 @@ export class CoffeaCirculor extends CoffeeShop implements CoffeeShopProperties {
     }
 
     return !prices.length;
-  }
-
-  async getPrice(page: Page) {
-    const sizes = await page.$$('[data-text^="250g"] span');
-
-    const prices = [];
-
-    for (const size of sizes) {
-      await size.click();
-      const isAddToCartDisabled = await page.$eval(
-        'button[name="add"]',
-        (button) => button.disabled,
-      );
-      if (!isAddToCartDisabled) {
-        const price = await page.$eval(
-          'span.product-price',
-          (el) => +el.textContent!.slice(1).replace(',', '') / 100,
-        );
-
-        prices.push(price);
-      }
-    }
-
-    return Math.min(...prices);
   }
 }
